0e1b69868f976745f17bd3d547d7151c95b379d4
Ahmed Yasser <ahmed.yassertaha@gmail.com>
2019-02-05 21:48:28 +0200

    Added sidebar UI

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React from 'react';
+import { Icon } from 'react-icons-kit';
+import { facebookSquare } from 'react-icons-kit/fa/facebookSquare';
+import { twitterSquare } from 'react-icons-kit/fa/twitterSquare';
+import { behanceSquare } from 'react-icons-kit/fa/behanceSquare';
+import { dribbble } from 'react-icons-kit/fa/dribbble'
 export default function SideBar() {
   return (
     <div id="sidebar">
-      <div style={{ width: '50%', borderRadius: '50%', overflow: 'hidden' }}>
-        <img src={require('../images/John-Doe.jpg')} style={{ width: '100%' }} alt="" />
+      <img src={require('../images/johndoe.jpg')} style={{ width: '40%' }} alt="" />
+      <p>~ JOHN DOE ~</p>
+      <p>johndoe@exmple.com</p>
+
+      <div class="contact">
+        <h3>contact</h3>
+        <div className="contact-item">
+          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={facebookSquare} />
+          <span>https://fb.com/johndoe</span>
+        </div>
+        <div className="contact-item">
+          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={twitterSquare} />
+          <span>https://twiiter.com/johndoe</span>
+        </div>
       </div>
+
+      <div class="contact">
+        <h3>projects</h3>
+        <div className="contact-item">
+          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={dribbble} />
+          <span>https://dribbble.com/johndoe</span>
+        </div>
+        <div className="contact-item">
+          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={behanceSquare} />
+          <span>https://behance.com/johndoe</span>
+        </div>
+      </div>
+
     </div>
   )
 }
