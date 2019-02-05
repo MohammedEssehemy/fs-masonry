0f555ba32e51c5c89f98607e4394d8167740b017
Ahmed Yasser <ahmed.yassertaha@gmail.com>
2019-02-05 22:30:45 +0200

    added responsive sidebar with a toggle button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,38 +3,64 @@ import { Icon } from 'react-icons-kit';
 import { facebookSquare } from 'react-icons-kit/fa/facebookSquare';
 import { twitterSquare } from 'react-icons-kit/fa/twitterSquare';
 import { behanceSquare } from 'react-icons-kit/fa/behanceSquare';
-import { dribbble } from 'react-icons-kit/fa/dribbble'
-export default function SideBar() {
-  return (
-    <div id="sidebar">
-      <img src={require('../images/johndoe.jpg')} style={{ width: '40%' }} alt="" />
-      <p>~ JOHN DOE ~</p>
-      <p>johndoe@exmple.com</p>
+import { dribbble } from 'react-icons-kit/fa/dribbble';
+import { user } from 'react-icons-kit/fa/user';
+export default class SideBar extends React.Component {
 
-      <div className="contact">
-        <h3>contact</h3>
-        <div className="contact-item">
-          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={facebookSquare} />
-          <span>https://fb.com/johndoe</span>
-        </div>
-        <div className="contact-item">
-          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={twitterSquare} />
-          <span>https://twiiter.com/johndoe</span>
-        </div>
-      </div>
+  sidebar = React.createRef();
+  toggler = React.createRef();
 
-      <div className="contact">
-        <h3>projects</h3>
-        <div className="contact-item">
-          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={dribbble} />
-          <span>https://dribbble.com/johndoe</span>
-        </div>
-        <div className="contact-item">
-          <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={behanceSquare} />
-          <span>https://behance.com/johndoe</span>
-        </div>
-      </div>
+  handleTogglerClick = () => {
+    const display = getComputedStyle(this.sidebar.current, null).display;
+    if (display === 'none') {
+      this.sidebar.current.style.display = 'block';
+      this.toggler.current.style.left = '75vw';
+    } else {
+      this.sidebar.current.style.display = 'none';
+      this.toggler.current.style.left = '5vw';
+    }
+  }
 
-    </div>
-  )
+  render() {
+    return (
+      <>
+        <div id="toggler" ref={this.toggler} onClick={this.handleTogglerClick}>
+          <Icon style={{ color: '#ccc' }} icon={user} size={30} />
+        </div>
+        <div id="sidebar" ref={this.sidebar}>
+          <div id="sidebar-content">
+            <img src={require('../images/johndoe.jpg')} style={{ width: '40%' }} alt="" />
+            <p>~ JOHN DOE ~</p>
+            <p>johndoe@exmple.com</p>
+
+            <div className="contact">
+              <h3>contact</h3>
+              <div className="contact-item">
+                <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={facebookSquare} />
+                <span>https://fb.com/johndoe</span>
+              </div>
+              <div className="contact-item">
+                <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={twitterSquare} />
+                <span>https://twiiter.com/johndoe</span>
+              </div>
+            </div>
+
+            <div className="contact">
+              <h3>projects</h3>
+              <div className="contact-item">
+                <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={dribbble} />
+                <span>https://dribbble.com/johndoe</span>
+              </div>
+              <div className="contact-item">
+                <Icon style={{ color: '#ccc', margin: '0 10px' }} icon={behanceSquare} />
+                <span>https://behance.com/johndoe</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </>
+
+    )
+  }
 }
