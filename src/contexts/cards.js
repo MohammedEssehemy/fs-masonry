115079a47e76509c196bdcd39655f0b1a0bb63f8
Mohammed Essehemy <mohammedessehemy@gmail.com>
2019-06-21 19:25:01 +0200

    Deploy to now

--- src/contexts/cards.js
+++ src/contexts/cards.js
@@ -15,7 +15,7 @@ export class CardsProvider extends PureComponent {
 
   fetchCards = async () => {
     this.setState({ loading: true });
-    const response = await fetch('http://www.amiiboapi.com/api/amiibo/');
+    const response = await fetch('https://www.amiiboapi.com/api/amiibo/');
     if (response.ok) {
       const data = await response.json();
       this.setState({ loading: false })
